b18cec3827e7a5d0223b5c14a70584ff1c7e2c62
Alex Pinkus <alex.pinkus@gmail.com>
2023-02-19 13:53:38 -0800

    Remove corpus files from old location
    Corpus files were moved under `test/` in the last commit. The checkin
    script won't change that for us, so we have to manually delete.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
       path: ".",
       exclude: [
         "bindings",
-        "corpus",
+        "test",
         "LICENSE",
         "package.json",
         "README.md",
