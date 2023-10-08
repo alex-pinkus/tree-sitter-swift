7e4ccc97a25315022a70b730085deccd5680a39b
Alex Pinkus <pinkus@amazon.com>
2023-10-08 16:55:43 -0700

    Update availability conditions to support #unavailable
    Fixes one of the failures from #323

--- grammar.js
+++ grammar.js
@@ -1175,7 +1175,12 @@ module.exports = grammar({
         )
       ),
     availability_condition: ($) =>
-      seq("#available", "(", sep1($._availability_argument, ","), ")"),
+      seq(
+        choice("#available", "#unavailable"),
+        "(",
+        sep1($._availability_argument, ","),
+        ")"
+      ),
     _availability_argument: ($) =>
       choice(seq($.identifier, sep1($.integer_literal, ".")), "*"),
     ////////////////////////////////
