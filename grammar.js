1f5ff37b4ea1c7fb5439e34f60f897fed0e7139c
Patrick Thomson <patrickt@github.com>
2024-03-14 12:51:13 -0400

    Prettier.

--- grammar.js
+++ grammar.js
@@ -1948,7 +1948,14 @@ module.exports = grammar({
       choice("override", "convenience", "required", "nonisolated"),
     visibility_modifier: ($) =>
       seq(
-        choice("public", "private", "internal", "fileprivate", "open", "package"),
+        choice(
+          "public",
+          "private",
+          "internal",
+          "fileprivate",
+          "open",
+          "package",
+        ),
         optional(seq("(", "set", ")"))
       ),
     type_parameter_modifiers: ($) => repeat1($.attribute),
