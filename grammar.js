999c5bb664947400daa670f4e93c2dab1600cff6
Patrick Thomson <patrickt@github.com>
2024-03-14 12:59:05 -0400

    Trailing comma unhappiness?

--- grammar.js
+++ grammar.js
@@ -1911,7 +1911,7 @@ module.exports = grammar({
           "internal",
           "fileprivate",
           "open",
-          "package",
+          "package"
         ),
         optional(seq("(", "set", ")"))
       ),
